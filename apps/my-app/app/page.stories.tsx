7b3bccaff467cf643c231869cce2a7832ba3db30
Harley Alexander <43975092+mayteio@users.noreply.github.com>
2023-07-07 08:06:36 +0000

    Update 16 files

--- apps/my-app/app/page.stories.tsx
+++ apps/my-app/app/page.stories.tsx
@@ -2,4 +2,4 @@ import Page from './page';
 
 export default { title: 'app/page', component: Page };
 
-export const Primary = { args: {}, storyName: 'page' };
+export const Primary = { args: {}, name: 'page' };
--- libs/test/jest.config.ts
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-export default {
-  displayName: 'test',
-  preset: '../../jest.preset.js',
-  coverageDirectory: '../../coverage/libs/test',
-};
