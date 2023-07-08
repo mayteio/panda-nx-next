19985c3813fc6f65ae6c09558e89a82d22e7d9fe
Harley Alexander <43975092+mayteio@users.noreply.github.com>
2023-07-08 12:06:03 +1000

    Draft/trusting ganguly (#3)
    * Update 16 files
    
    * remove generated files
    
    * Update 7 files

--- libs/theme/src/button.ts
+++ libs/theme/src/button.ts
@@ -3,11 +3,65 @@ import { defineRecipe } from '@pandacss/dev';
 export const button = defineRecipe({
   name: 'button',
   base: {
-    px: 4,
-    py: 3,
-    bg: 'black',
-    color: 'white',
-    cursor: 'pointer',
     borderRadius: '8px',
+    color: 'reverse-heavy',
+    cursor: 'pointer',
+    position: 'relative',
+    display: 'flex',
+    alignItems: 'center',
+    fontWeight: 600,
+    transitionProperty: 'background, border-color',
+    transitionDuration: '150ms',
+    _disabled: {
+      cursor: 'not-allowed',
+    },
+    _focus: {
+      outline: 'none',
+      _active: { _after: { display: 'none!' } },
+      _after: {
+        content: '""',
+        border: '2px solid token(colors.heavy)',
+        borderRadius: '12px',
+        width: 'calc(100% + 8px)',
+        height: 'calc(100% + 8px)',
+        position: 'absolute',
+        left: '-4px',
+        top: '-4px',
+      },
+    },
+  },
+  variants: {
+    visual: {
+      branded: { bgGradient: 'brand' },
+      default: {
+        bg: 'heavy',
+        _disabled: { bg: 'medium', color: 'reverse-light' },
+      },
+      destructive: { bg: 'error' },
+      secondary: {
+        border: '1px solid token(colors.light)',
+        color: 'heavy',
+        _hover: { bg: 'reverse-heavy' },
+        _focus: {
+          border: '3px solid token(colors.heavy)',
+          _after: { display: 'none' },
+        },
+      },
+      tertiary: { color: 'heavy', textDecoration: 'underline' },
+    },
+    size: {
+      sm: { px: '16px', fontSize: '14px', height: '32px' },
+      md: { px: '24px', fontSize: '16px', height: '48px' },
+      lg: { px: '32px', fontSize: '18px', height: '56px' },
+    },
+  },
+  compoundVariants: [
+    { visual: 'secondary', size: 'sm', css: { _focus: { px: '14px' } } },
+    { visual: 'secondary', size: 'md', css: { _focus: { px: '22px' } } },
+    { visual: 'secondary', size: 'lg', css: { _focus: { px: '30px' } } },
+  ],
+  defaultVariants: {
+    visual: 'default',
+    size: 'md',
   },
 });
--- libs/theme/src/tokens.ts
+++ libs/theme/src/tokens.ts
@@ -1,7 +1,79 @@
-import { defineTokens } from '@pandacss/dev';
+import { defineSemanticTokens, defineTokens } from '@pandacss/dev';
 
 export const tokens = defineTokens({
   colors: {
-    peach: { value: 'peachpuff' },
+    monochrome: {
+      50: { value: '#FFF' },
+      100: { value: '#F7F7F766' },
+      200: { value: '#F7F7F799' },
+      300: { value: '#F7F7F7' },
+      400: { value: '#EBEBEB' },
+      500: { value: '#B0B0B0' },
+      600: { value: '#22222266' },
+      700: { value: '#22222299' },
+      800: { value: '#222' },
+      900: { value: '#0F0F0F' },
+    },
+    green: { value: '#4DBC5D' },
+    orange: { value: '#FF9B53' },
+    coral: { value: '#FA635D' },
+  },
+  gradients: {
+    // string value
+    brand: { value: 'linear-gradient(to right, #08AABF, #4DBC5D)' },
+  },
+  fonts: {
+    branded: { value: 'var(--font-recoleta), Menlo, monospace' },
+    default: { value: 'var(--font-proxima-nova), sans-serif' },
+  },
+});
+
+export const semanticTokens = defineSemanticTokens({
+  colors: {
+    heavy: {
+      value: {
+        base: '{colors.monochrome.800}',
+        _dark: '{colors.monochrome.300}',
+      },
+    },
+    medium: {
+      value: {
+        base: '{colors.monochrome.700}',
+        _dark: '{colors.monochrome.200}',
+      },
+    },
+    light: {
+      value: {
+        base: '{colors.monochrome.600}',
+        _dark: '{colors.monochrome.100}',
+      },
+    },
+    'reverse-heavy': {
+      value: {
+        base: '{colors.monochrome.300}',
+        _dark: '{colors.monochrome.800}',
+      },
+    },
+    'reverse-medium': {
+      value: {
+        base: '{colors.monochrome.200}',
+        _dark: '{colors.monochrome.700}',
+      },
+    },
+    'reverse-light': {
+      value: {
+        base: '{colors.monochrome.100}',
+        _dark: '{colors.monochrome.600}',
+      },
+    },
+    success: {
+      value: { base: '{colors.green}', _dark: '{colors.green}' },
+    },
+    warning: {
+      value: { base: '{colors.orange}', _dark: '{colors.orange}' },
+    },
+    error: {
+      value: { base: '{colors.coral}', _dark: '{colors.coral}' },
+    },
   },
 });
